4a15ff0227835cb843732a8535344bd37380b4ff
Yong Kok Khuen <17147279@siswa.um.edu.my>
2022-04-21 23:39:40 +0800

    Add utils module

---
+++ modeling/utils/metrics.py
@@ -0,0 +1,25 @@
+from asyncio.windows_events import NULL
+import pickle
+import matplotlib.pyplot as plt
+from sklearn.metrics import classification_report, ConfusionMatrixDisplay
+
+y_test = NULL
+
+
+def evaluate(y_pred):
+    global y_test
+
+    if y_test == NULL:
+        y_test = load_test_labels()
+
+    print("Classification Report:")
+    print(classification_report(y_test, y_pred))
+
+    print("Confusion Matrix:")
+    ConfusionMatrixDisplay.from_predictions(y_test, y_pred)
+    plt.show()
+
+
+def load_test_labels():
+    with open("../data/y_test.pickle", "rb") as f:
+        return pickle.load(f)
